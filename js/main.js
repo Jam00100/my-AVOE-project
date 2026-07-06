c261665f854ba8eff5b0c61ae0e1072164ece1df
James <james@JamesdeMacBook-Pro.local>
2026-07-06 14:57:31 +0800

    加入跳轉商品網頁功能

--- js/main.js
+++ js/main.js
@@ -13,10 +13,12 @@ const productGrid = document.getElementById("product-grid");
 function createProductCard(product) {
 	return `
 		<div class="product-card">
-			<img src="${product.image}" alt="${product.name}">
-			<p class="product-category"> ${product.category} </p>
-			<h3 class="product-title">${product.name} </h3>
-			<p class="product-price"> ${product.price} </p>
+			<a href="product.html?id=${product.id}">
+				<img src="${product.image}" alt="${product.name}">
+				<p class="product-category"> ${product.category} </p>
+				<h3 class="product-title">${product.name} </h3>
+				<p class="product-price"> ${product.price} </p>
+			</a>
 		</div>
 	`;
 }
